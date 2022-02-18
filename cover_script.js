989f0f94f7029612c7d33bf58850e1ed52df331e
bibek10550 <84659279+bibek10550@users.noreply.github.com>
2022-02-18 17:37:27 +0545

    Update cover_script.js

--- cover_script.js
+++ cover_script.js
@@ -2,3 +2,8 @@ var preloader = document.getElementById('loading');
 function myFunction(){
     preloader.style.display = 'none';
 }
+
+setTimeout(logoutUser, 60000); // 5000=5s
+function logoutUser(){
+    window.location = "index.html";
+}
